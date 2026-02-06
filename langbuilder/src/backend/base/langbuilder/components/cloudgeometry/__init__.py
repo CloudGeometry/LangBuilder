97e28730de998f29963961f0c3c3f6b3aa6803ec
anreilspec <andreil@cloudgeometry.com>
2026-02-06 14:50:00 +0200

    feat: add dynamo component

--- langbuilder/src/backend/base/langbuilder/components/cloudgeometry/__init__.py
+++ langbuilder/src/backend/base/langbuilder/components/cloudgeometry/__init__.py
@@ -5,59 +5,26 @@ from typing import TYPE_CHECKING, Any
 from langbuilder.components._importing import import_mod
 
 if TYPE_CHECKING:
-    from langbuilder.components.cloudgeometry.jinja2_renderer import Jinja2Renderer
     from langbuilder.components.cloudgeometry.contact_info_extractor import ContactInfoExtractorComponent
+    from langbuilder.components.cloudgeometry.dynamodb_pm_config_reader import DynamoDBPMConfigReaderComponent
+    from langbuilder.components.cloudgeometry.jinja2_renderer import Jinja2Renderer
     from langbuilder.components.cloudgeometry.pinecone_search_tool import PineconeSearchToolComponent
     from langbuilder.components.cloudgeometry.pinecone_store_tool import PineconeStoreToolComponent
-    from langbuilder.components.cloudgeometry.dummy_component import CloudGeometryDummyComponent
-    from langbuilder.components.cloudgeometry.atlassian_mcp_component import AtlassianMCPComponent
-    from langbuilder.components.cloudgeometry.action_plan_store import ActionPlanStoreComponent
-    from langbuilder.components.cloudgeometry.slack_block_kit_poster import SlackBlockKitPosterComponent
-    from langbuilder.components.cloudgeometry.slack_event_parser import SlackEventParserComponent
-    from langbuilder.components.cloudgeometry.slack_dashboard_updater import SlackDashboardUpdaterComponent
-    from langbuilder.components.cloudgeometry.per_user_jira_auth import PerUserJiraAuthComponent
-    from langbuilder.components.cloudgeometry.slack_modal_trigger import SlackModalTriggerComponent
-    from langbuilder.components.cloudgeometry.slack_component import SlackComponent
-    from langbuilder.components.cloudgeometry.jira_state_fetcher import JiraStateFetcherComponent
-    from langbuilder.components.cloudgeometry.jira_writer_tools import JiraWriterToolsComponent
-    from langbuilder.components.cloudgeometry.jira_reader_writer import JiraReaderWriterComponent
 
 _dynamic_imports = {
-    "Jinja2Renderer": "jinja2_renderer",
     "ContactInfoExtractorComponent": "contact_info_extractor",
+    "DynamoDBPMConfigReaderComponent": "dynamodb_pm_config_reader",
+    "Jinja2Renderer": "jinja2_renderer",
     "PineconeSearchToolComponent": "pinecone_search_tool",
     "PineconeStoreToolComponent": "pinecone_store_tool",
-    "CloudGeometryDummyComponent": "dummy_component",
-    "AtlassianMCPComponent": "atlassian_mcp_component",
-    "ActionPlanStoreComponent": "action_plan_store",
-    "SlackBlockKitPosterComponent": "slack_block_kit_poster",
-    "SlackEventParserComponent": "slack_event_parser",
-    "SlackDashboardUpdaterComponent": "slack_dashboard_updater",
-    "PerUserJiraAuthComponent": "per_user_jira_auth",
-    "SlackModalTriggerComponent": "slack_modal_trigger",
-    "SlackComponent": "slack_component",
-    "JiraStateFetcherComponent": "jira_state_fetcher",
-    "JiraWriterToolsComponent": "jira_writer_tools",
-    "JiraReaderWriterComponent": "jira_reader_writer",
 }
 
 __all__ = [
-    "Jinja2Renderer",
     "ContactInfoExtractorComponent",
+    "DynamoDBPMConfigReaderComponent",
+    "Jinja2Renderer",
     "PineconeSearchToolComponent",
     "PineconeStoreToolComponent",
-    "CloudGeometryDummyComponent",
-    "AtlassianMCPComponent",
-    "ActionPlanStoreComponent",
-    "SlackBlockKitPosterComponent",
-    "SlackEventParserComponent",
-    "SlackDashboardUpdaterComponent",
-    "PerUserJiraAuthComponent",
-    "SlackModalTriggerComponent",
-    "SlackComponent",
-    "JiraStateFetcherComponent",
-    "JiraWriterToolsComponent",
-    "JiraReaderWriterComponent",
 ]
 
 
